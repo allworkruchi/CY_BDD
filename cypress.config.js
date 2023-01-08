2023c096b9ae1cc2b63d5ec456e64444d37407f9
allworkruchi <allworkruchi@gmail.com>
2023-01-08 14:33:11 +0530

    my initial coimmit

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require("cypress");
+const cucumber = require('cypress-cucumber-preprocessor').default
+
+module.exports = defineConfig({
+  
+  "video": false,
+  "defaultCommandTimeout": 5000,
+  "pageLoadTimeout": 10000,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      on('file:preprocessor',cucumber())
+    },
+      specPattern: '**/*.feature',
+      excludeSpecPattern: ['*.js'],
+  },
+});
---
+++ cypress/e2e/login.feature
@@ -0,0 +1,8 @@
+Feature: Login to application 
+
+Background: Valid login
+ Given I open login page
+ When I submit login
+ Then I should see homepage
+
+ 
